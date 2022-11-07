f81652768d3ea462949794a1970ab6382e9ff35f
fatma1 <fatma.araissia@korsgy.com>
2022-11-07 18:39:48 +0100

    Facture null pointer

--- src/main/java/com/esprit/examen/services/FactureServiceImpl.java
+++ src/main/java/com/esprit/examen/services/FactureServiceImpl.java
@@ -147,7 +147,7 @@ public class FactureServiceImpl implements IFactureService {
 	@Override
 	public void assignOperateurToFacture(Long idOperateur, Long idFacture) {
 		Facture facture = factureRepository.findById(idFacture).orElse(null);
-		Operateur operateur = null;
+		Operateur operateur = new Operateur();
 		operateur = operateurRepository.findById(idOperateur).orElse(null);
 		operateur.getFactures().add(facture);
 		operateurRepository.save(operateur);
